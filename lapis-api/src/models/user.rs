34d3c378f6c1daced47d240995f8f197cf020d1f
Tyler Harold <tyler.harold@nitrado.net>
2023-04-30 10:50:52 -0400

    feat(lapis-api/models): add user models

---
+++ lapis-api/src/models/user.rs
@@ -0,0 +1,34 @@
+use chrono::prelude::*;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize, sqlx::FromRow, Serialize, Clone)]
+pub struct User {
+    pub id: uuid::Uuid,
+    pub name: String,
+    pub email: String,
+    pub passord: String,
+    pub role: String,
+    pub photo: String,
+    pub created_at: Option<DateTime<Utc>>,
+    pub updated_at: Option<DateTime<Utc>>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct TokenClaims {
+    pub sub: String,
+    pub iat: usize,
+    pub exp: usize,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct RegisterUserSchema {
+    pub name: String,
+    pub email: String,
+    pub password: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct LoginUserSchema {
+    pub email: String,
+    pub password: String,
+}
